9dea21e19935fa28782be3d5ec8a537efbe357a1
Midori Yang <midoki.yang@gmail.com>
2020-11-16 02:11:04 -0500

    able to identify which repos need to be updated

--- utils/updateEslint.js
+++ utils/updateEslint.js
@@ -1,41 +1,53 @@
 const path = require('path');
+require('colors');
 const { Repository, Diff, DiffOptions } = require('nodegit');
-const { exec } = require('child_process');
 const UnitConfig = require('../unit-config');
 const eslint = require('../unit-config/eslintConstants.js');
-
-const execBash = (command, cb) => {
-  if (!cb) exec(command, { shell: 'bash' });
-  else exec(command, { shell: 'bash' }, cb);
-};
+const { Console } = require('console');
 
 const ROOT = path.resolve(__dirname, '..');
-console.log(ROOT);
 
 (async function () {
-  // figure out which eslintrcs have diffs
+  /*
+   * Figure out which eslint templates have diffs
+   */
   const repo = await Repository.open(`${ROOT}`);
-  console.log('sha', (await repo.getHeadCommit()).sha());
+  // get a reference to the file state (tree) of most recent commit
   const headTree = await repo.getHeadCommit().then((head) => head.getTree());
+  // only compare files in the eslint templates folder
   const diffOptions = new DiffOptions();
   diffOptions.pathspec = `unit-config/eslint-boilerplates`;
-  const indexDiff = await Diff.indexToWorkdir(repo, null, diffOptions);
-  const treeDiff = await Diff.treeToWorkdirWithIndex(repo, headTree, diffOptions);
-  const indexPatches = await indexDiff.patches();
+  // get the diff between the repo tree and the working directory tree
+  const treeDiff = await Diff.treeToWorkdir(repo, headTree, diffOptions);
+  // parse the list of diffs and get the eslint file names
   const treePatches = await treeDiff.patches();
-  indexPatches.forEach(async (patch) => {
-    console.log('index', patch.newFile().path());
-    patch.hunks().then((hunks) => {
-      hunks.forEach((hunk) => console.log(hunk.header()));
-    });
+  const updatedFiles = treePatches.map((patch) => {
+    const paths = patch.oldFile().path().split('/');
+    const file = paths[paths.length - 1];
+    return file;
   });
-  treePatches.forEach((patch) => {
-    console.log('tree', patch.oldFile().path());
-    patch.hunks().then((hunks) => {
-      hunks.forEach((hunk) => console.log(hunk.header()));
-    });
+  if (!updatedFiles.length) {
+    return console.log('No changes found in any eslint config files.'.red);
+  }
+  console.log('Changes found in these eslint config files:'.yellow);
+  updatedFiles.forEach((file) => console.log(file));
+  console.log('\n');
+
+  /*
+   * Check which units list those eslintrcs in their configs
+   */
+  const unitConfig = UnitConfig.readSync();
+  console.log('Units that need to be updated:'.yellow);
+  const unitsToUpdate = Object.keys(unitConfig).filter((unit) => {
+    for (let i = 0; i < updatedFiles.length; i++) {
+      if (unitConfig[unit].eslint.includes(updatedFiles[i])) {
+        console.log(unit);
+        return true;
+      }
+    }
+    console.log(unit.grey);
+    return false;
   });
-  // check which units list those eslintrcs in their configs
   // for each unit repo
   // create a feature branch
   // generate new eslintrc based on the templates
